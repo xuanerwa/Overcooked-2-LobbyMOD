de5d863abfcd91f9e3fa95abd103ad2875ba44d6
CH3NG <xiaochengzi0105@gmail.com>
2024-04-17 17:06:42 +0800

    测试替换后的自动打包压缩方法

--- HostUtilities/HostUtilities/_MODEntry.cs
+++ HostUtilities/HostUtilities/_MODEntry.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using Team17.Online;
 using UnityEngine;
- 
+
 namespace HostUtilities
 {
     [BepInPlugin("com.ch3ngyz.plugin.HostUtilities", "[HostUtilities] By.yc阿哲 Q群860480677 点击下方“‧‧‧”展开", "1.0.40")]
