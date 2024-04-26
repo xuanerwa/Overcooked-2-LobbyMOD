23bbcd51835bafca47e2193cd2aa348437db5283
CYZ <2927243119@qq.com>
2024-04-26 17:30:32 +0800

    修复 当两个以上的物体卡在同一个格子上, 会导致大量掉帧的问题

--- HostUtilities/HostUtilities/FixBrokenWashingStation.cs
+++ HostUtilities/HostUtilities/FixBrokenWashingStation.cs
@@ -8,11 +8,13 @@ namespace HostUtilities
     public class FixBrokenWashingStation
     {
         public static Harmony HarmonyInstance { get; set; }
+        public static string currentString;
+        public static ConfigEntry<bool> showAttachSituation;
         //public static ConfigEntry<bool> isFixBrokenWashingStation;
         public static void log(string mes) => _MODEntry.LogInfo(mes);
         public static void Awake()
         {
-            //isFixBrokenWashingStation = _MODEntry.Instance.Config.Bind("01-功能开关", "修复被砸坏的洗碗池", true);
+            showAttachSituation = _MODEntry.Instance.Config.Bind("00-UI", "07-Console中显示Attatch详情", false, "如您不知道此选项是干嘛的, 请不要开启, 若开启, 当两个以上的物体卡在同一个格子上, 会导致大量掉帧");
             HarmonyInstance = Harmony.CreateAndPatchAll(MethodBase.GetCurrentMethod().DeclaringType);
             _MODEntry.AllHarmony.Add(HarmonyInstance);
             _MODEntry.AllHarmonyName.Add(MethodBase.GetCurrentMethod().DeclaringType.Name);
@@ -22,13 +24,16 @@ namespace HostUtilities
         [HarmonyPatch(typeof(ServerAttachStation), "CanAttachToSelf")]
         public static bool ServerAttachStation_CanAttachToSelf_Prefix(ref ServerAttachStation __instance, UnityEngine.GameObject _item, PlacementContext _context)
         {
-            if (__instance.name == "WashingPart")
+            if (__instance.name == "WashingPart" || __instance.name == "Switch(1)" || __instance.name == "workstation_cooker_01")
             {
                 return false;
             }
             else
             {
-                log($"CanAttachToSelf: InstanceName: 【{__instance.name}】, ObjectName: 【{_item.name}】");
+                if (showAttachSituation.Value)
+                {
+                    log($"CanAttachToSelf: InstanceName: 【{__instance.name}】, ObjectName: 【{_item.name}】");
+                }
                 return true;
             }
         }
